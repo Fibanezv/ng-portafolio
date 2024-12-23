13e12c1646a95aceb54aeb01d18f3e8d0e2477cd
Fran Ibañez <ibanezvarelafran@gmail.com>
2024-12-23 19:32:53 +0100

    routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,7 @@ export const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
+    useHash: false,
     scrollPositionRestoration: 'enabled', 
     anchorScrolling: 'enabled',
   })],
